7e6049a44204b58c94b8366f4d0111339132e150
MrBrdo <mrbrdo@zephyrus.mrbrdo.net>
2010-10-15 17:16:11 +0200

    fix bug and readme

--- lib/generators/templates/model.rb
+++ lib/generators/templates/model.rb
@@ -6,8 +6,8 @@ class AnonymousUser < ActiveRecord::Base
       u = self.find_by_cookie_token(cookies["anon_user_token"])
     end
     unless u
-      cookies["anon_user_token"] = Time.now.to_i.to_s + rand(1000).to_s + "_" + request.remote_ip
-      u = self.create({:ip => request.remote_ip, :cookie_token => cookies["anon_user_token"]})
+      cookies["anon_user_token"] = Time.now.to_i.to_s + rand(1000).to_s + "_" + ip
+      u = self.create({:ip => ip, :cookie_token => cookies["anon_user_token"]})
     end
     u
   end
